431bd1e004e1c6f0c1f25f487b1aec48ea75aa5e
Kieeen <111482862+Kieeen@users.noreply.github.com>
2022-09-05 16:55:15 +0700

    Add files via upload
    Week1-Caculator

---
+++ My Calculator/My Calculator/Form1.Designer.cs
@@ -0,0 +1,281 @@
+﻿namespace My_Calculator
+{
+    partial class MyCalculator
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtDisplay = new System.Windows.Forms.TextBox();
+            this.nutC = new System.Windows.Forms.Button();
+            this.nut9 = new System.Windows.Forms.Button();
+            this.nut8 = new System.Windows.Forms.Button();
+            this.nut7 = new System.Windows.Forms.Button();
+            this.nutT = new System.Windows.Forms.Button();
+            this.nut6 = new System.Windows.Forms.Button();
+            this.nut5 = new System.Windows.Forms.Button();
+            this.nut4 = new System.Windows.Forms.Button();
+            this.nutN = new System.Windows.Forms.Button();
+            this.nut3 = new System.Windows.Forms.Button();
+            this.nut2 = new System.Windows.Forms.Button();
+            this.nut1 = new System.Windows.Forms.Button();
+            this.NutChia = new System.Windows.Forms.Button();
+            this.nut0 = new System.Windows.Forms.Button();
+            this.NutOk = new System.Windows.Forms.Button();
+            this.NutCl = new System.Windows.Forms.Button();
+            this.NutCham = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtDisplay
+            // 
+            this.txtDisplay.Location = new System.Drawing.Point(53, 51);
+            this.txtDisplay.Multiline = true;
+            this.txtDisplay.Name = "txtDisplay";
+            this.txtDisplay.Size = new System.Drawing.Size(270, 68);
+            this.txtDisplay.TabIndex = 37;
+            this.txtDisplay.Text = "0";
+            this.txtDisplay.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.txtDisplay.TextChanged += new System.EventHandler(this.txtDisplay_TextChanged);
+            // 
+            // nutC
+            // 
+            this.nutC.Location = new System.Drawing.Point(231, 149);
+            this.nutC.Name = "nutC";
+            this.nutC.Size = new System.Drawing.Size(34, 37);
+            this.nutC.TabIndex = 36;
+            this.nutC.Text = "+";
+            this.nutC.UseVisualStyleBackColor = true;
+            this.nutC.Click += new System.EventHandler(this.nutC_Click);
+            // 
+            // nut9
+            // 
+            this.nut9.Location = new System.Drawing.Point(173, 149);
+            this.nut9.Name = "nut9";
+            this.nut9.Size = new System.Drawing.Size(34, 37);
+            this.nut9.TabIndex = 35;
+            this.nut9.Text = "9";
+            this.nut9.UseVisualStyleBackColor = true;
+            this.nut9.Click += new System.EventHandler(this.nut9_Click);
+            // 
+            // nut8
+            // 
+            this.nut8.Location = new System.Drawing.Point(111, 149);
+            this.nut8.Name = "nut8";
+            this.nut8.Size = new System.Drawing.Size(34, 37);
+            this.nut8.TabIndex = 34;
+            this.nut8.Text = "8";
+            this.nut8.UseVisualStyleBackColor = true;
+            this.nut8.Click += new System.EventHandler(this.nut8_Click);
+            // 
+            // nut7
+            // 
+            this.nut7.Location = new System.Drawing.Point(53, 149);
+            this.nut7.Name = "nut7";
+            this.nut7.Size = new System.Drawing.Size(34, 37);
+            this.nut7.TabIndex = 33;
+            this.nut7.Text = "7";
+            this.nut7.UseVisualStyleBackColor = true;
+            this.nut7.Click += new System.EventHandler(this.nut7_Click);
+            // 
+            // nutT
+            // 
+            this.nutT.Location = new System.Drawing.Point(231, 206);
+            this.nutT.Name = "nutT";
+            this.nutT.Size = new System.Drawing.Size(34, 37);
+            this.nutT.TabIndex = 32;
+            this.nutT.Text = "-";
+            this.nutT.UseVisualStyleBackColor = true;
+            // 
+            // nut6
+            // 
+            this.nut6.Location = new System.Drawing.Point(173, 206);
+            this.nut6.Name = "nut6";
+            this.nut6.Size = new System.Drawing.Size(34, 37);
+            this.nut6.TabIndex = 31;
+            this.nut6.Text = "6";
+            this.nut6.UseVisualStyleBackColor = true;
+            this.nut6.Click += new System.EventHandler(this.nut6_Click);
+            // 
+            // nut5
+            // 
+            this.nut5.Location = new System.Drawing.Point(111, 206);
+            this.nut5.Name = "nut5";
+            this.nut5.Size = new System.Drawing.Size(34, 37);
+            this.nut5.TabIndex = 30;
+            this.nut5.Text = "5";
+            this.nut5.UseVisualStyleBackColor = true;
+            this.nut5.Click += new System.EventHandler(this.nut5_Click);
+            // 
+            // nut4
+            // 
+            this.nut4.Location = new System.Drawing.Point(53, 206);
+            this.nut4.Name = "nut4";
+            this.nut4.Size = new System.Drawing.Size(34, 37);
+            this.nut4.TabIndex = 29;
+            this.nut4.Text = "4";
+            this.nut4.UseVisualStyleBackColor = true;
+            this.nut4.Click += new System.EventHandler(this.nut4_Click);
+            // 
+            // nutN
+            // 
+            this.nutN.Location = new System.Drawing.Point(231, 260);
+            this.nutN.Name = "nutN";
+            this.nutN.Size = new System.Drawing.Size(34, 37);
+            this.nutN.TabIndex = 28;
+            this.nutN.Text = "*";
+            this.nutN.UseVisualStyleBackColor = true;
+            // 
+            // nut3
+            // 
+            this.nut3.Location = new System.Drawing.Point(173, 260);
+            this.nut3.Name = "nut3";
+            this.nut3.Size = new System.Drawing.Size(34, 37);
+            this.nut3.TabIndex = 27;
+            this.nut3.Text = "3";
+            this.nut3.UseVisualStyleBackColor = true;
+            this.nut3.Click += new System.EventHandler(this.nut3_Click);
+            // 
+            // nut2
+            // 
+            this.nut2.Location = new System.Drawing.Point(111, 260);
+            this.nut2.Name = "nut2";
+            this.nut2.Size = new System.Drawing.Size(34, 37);
+            this.nut2.TabIndex = 26;
+            this.nut2.Text = "2";
+            this.nut2.UseVisualStyleBackColor = true;
+            this.nut2.Click += new System.EventHandler(this.nut2_Click);
+            // 
+            // nut1
+            // 
+            this.nut1.Location = new System.Drawing.Point(53, 260);
+            this.nut1.Name = "nut1";
+            this.nut1.Size = new System.Drawing.Size(34, 37);
+            this.nut1.TabIndex = 25;
+            this.nut1.Text = "1";
+            this.nut1.UseVisualStyleBackColor = true;
+            this.nut1.Click += new System.EventHandler(this.nut1_Click);
+            // 
+            // NutChia
+            // 
+            this.NutChia.Location = new System.Drawing.Point(231, 318);
+            this.NutChia.Name = "NutChia";
+            this.NutChia.Size = new System.Drawing.Size(34, 37);
+            this.NutChia.TabIndex = 24;
+            this.NutChia.Text = "/";
+            this.NutChia.UseVisualStyleBackColor = true;
+            // 
+            // nut0
+            // 
+            this.nut0.Location = new System.Drawing.Point(111, 318);
+            this.nut0.Name = "nut0";
+            this.nut0.Size = new System.Drawing.Size(34, 37);
+            this.nut0.TabIndex = 22;
+            this.nut0.Text = "0";
+            this.nut0.UseVisualStyleBackColor = true;
+            this.nut0.Click += new System.EventHandler(this.nut0_Click);
+            // 
+            // NutOk
+            // 
+            this.NutOk.Location = new System.Drawing.Point(289, 206);
+            this.NutOk.Name = "NutOk";
+            this.NutOk.Size = new System.Drawing.Size(34, 37);
+            this.NutOk.TabIndex = 21;
+            this.NutOk.Text = "=";
+            this.NutOk.UseVisualStyleBackColor = true;
+            this.NutOk.Click += new System.EventHandler(this.NutOk_Click);
+            // 
+            // NutCl
+            // 
+            this.NutCl.Location = new System.Drawing.Point(289, 149);
+            this.NutCl.Name = "NutCl";
+            this.NutCl.Size = new System.Drawing.Size(34, 37);
+            this.NutCl.TabIndex = 38;
+            this.NutCl.Text = "C";
+            this.NutCl.UseVisualStyleBackColor = true;
+            this.NutCl.Click += new System.EventHandler(this.NutCl_Click);
+            // 
+            // NutCham
+            // 
+            this.NutCham.Location = new System.Drawing.Point(173, 318);
+            this.NutCham.Name = "NutCham";
+            this.NutCham.Size = new System.Drawing.Size(34, 37);
+            this.NutCham.TabIndex = 39;
+            this.NutCham.Text = ".";
+            this.NutCham.UseVisualStyleBackColor = true;
+            this.NutCham.Click += new System.EventHandler(this.NutCham_Click);
+            // 
+            // MyCalculator
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(383, 415);
+            this.Controls.Add(this.NutCham);
+            this.Controls.Add(this.NutCl);
+            this.Controls.Add(this.txtDisplay);
+            this.Controls.Add(this.nutC);
+            this.Controls.Add(this.nut9);
+            this.Controls.Add(this.nut8);
+            this.Controls.Add(this.nut7);
+            this.Controls.Add(this.nutT);
+            this.Controls.Add(this.nut6);
+            this.Controls.Add(this.nut5);
+            this.Controls.Add(this.nut4);
+            this.Controls.Add(this.nutN);
+            this.Controls.Add(this.nut3);
+            this.Controls.Add(this.nut2);
+            this.Controls.Add(this.nut1);
+            this.Controls.Add(this.NutChia);
+            this.Controls.Add(this.nut0);
+            this.Controls.Add(this.NutOk);
+            this.Name = "MyCalculator";
+            this.Text = "My Calculator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtDisplay;
+        private System.Windows.Forms.Button nutC;
+        private System.Windows.Forms.Button nut9;
+        private System.Windows.Forms.Button nut8;
+        private System.Windows.Forms.Button nut7;
+        private System.Windows.Forms.Button nutT;
+        private System.Windows.Forms.Button nut6;
+        private System.Windows.Forms.Button nut5;
+        private System.Windows.Forms.Button nut4;
+        private System.Windows.Forms.Button nutN;
+        private System.Windows.Forms.Button nut3;
+        private System.Windows.Forms.Button nut2;
+        private System.Windows.Forms.Button nut1;
+        private System.Windows.Forms.Button NutChia;
+        private System.Windows.Forms.Button nut0;
+        private System.Windows.Forms.Button NutOk;
+        private System.Windows.Forms.Button NutCl;
+        private System.Windows.Forms.Button NutCham;
+    }
+}
+
---
+++ My Calculator/My Calculator/Form1.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace My_Calculator
+{
+   
+    public partial class MyCalculator : Form
+    {
+        private int so1 = 0;
+                int so2 = 0;
+        public MyCalculator()
+        {
+            InitializeComponent();
+        }
+
+        private void nut0_Click(object sender, EventArgs e)
+        {
+            if (txtDisplay.Text != "0")
+            {
+                txtDisplay.Text = "0";
+            }
+           
+        }
+
+        private void AddToTextDisplay(string p)
+        {
+            if (txtDisplay.Text == "0")
+            {
+                txtDisplay.Text = p;
+            }
+            else
+            {
+                txtDisplay.Text += p;
+            }
+        }
+
+        private void txtDisplay_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void nut1_Click(object sender, EventArgs e)
+        {
+            AddToTextDisplay("1");
+        }
+
+        private void nut2_Click(object sender, EventArgs e)
+        {
+            AddToTextDisplay("2");
+        }
+        private void nut3_Click(object sender, EventArgs e)
+        {
+            AddToTextDisplay("3");
+        }
+
+        private void nut4_Click(object sender, EventArgs e)
+        {
+            AddToTextDisplay("4");
+        }
+
+        private void nut5_Click(object sender, EventArgs e)
+        {
+            AddToTextDisplay("5");
+        }
+
+        private void nut6_Click(object sender, EventArgs e)
+        {
+            AddToTextDisplay("6");
+        }
+
+        private void nut7_Click(object sender, EventArgs e)
+        {
+            AddToTextDisplay("7");
+        }
+
+        private void nut8_Click(object sender, EventArgs e)
+        {
+            AddToTextDisplay("8");
+        }
+
+        private void nut9_Click(object sender, EventArgs e)
+        {
+            AddToTextDisplay("9");
+        }
+
+        private void nutC_Click(object sender, EventArgs e)
+        {
+            so1 = int.Parse(txtDisplay.Text);
+            txtDisplay.Text = "0";
+        }
+
+        private void NutOk_Click(object sender, EventArgs e)
+        {
+            so2 = int.Parse(txtDisplay.Text);
+            int kq = so1 + so2;
+            txtDisplay.Text = kq.ToString();
+        }
+
+        private void NutCl_Click(object sender, EventArgs e)
+        {
+            txtDisplay.Text = "0";
+        }
+
+        private void NutCham_Click(object sender, EventArgs e)
+        {
+            AddToTextDisplay(".");
+        }
+
+      
+    }
+}
